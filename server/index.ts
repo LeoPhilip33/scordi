715fdaea2aa9b8ddeb14f8699b490c45cdda66a7
Mickael Brunet <mickael.brunet64@gmail.com>
2022-10-28 14:58:37 +0200

    testing db connection and add folders

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,23 @@
 require('dotenv').config();
 import express from 'express';
 import bodyParser from "body-parser";
+import sequelize from './database';
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:true}))
 
 const PORT = process.env.PORT || 5000;
+
+const testingDbConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testingDbConnection()
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`)
